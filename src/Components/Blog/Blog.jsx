4c81534b4cc4f0a0a7d71f183906d1e077392e45
FAHIM-33 <shahriyerfahim2012@gmail.com>
2023-09-12 03:28:06 +0600

    added blogs

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,9 +1,29 @@
 import pt from 'prop-types'
-const Blog = ({blog}) => {
-    let {title} = blog;
+const Blog = ({ blog }) => {
+    let { title, cover, author, author_img, posted_date, reading_time, hash_tags } = blog;
     return (
-        <div>
-    <h1>{title}</h1>            
+        <div className='mb-20'>
+
+            <img className='rounded-lg' src={cover} alt="" />
+            <div className='flex justify-between items-center my-9'>
+                <div className='flex gap-6'>
+                    <figure className='w-14'>
+                        <img src={author_img} alt="" />
+                    </figure>
+                    <div>
+                        <h3 className='text-2xl font-bold'>{author}</h3>
+                        <p className='font-semibold text-gray-500'>Posted: {posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <p className='font-medium text-xl text-gray-500'>{reading_time} mins read</p>
+                </div>
+            </div>
+            <h2 className='text-4xl font-bold'>{title}</h2>
+            <p className='font-medium text-xl text-gray-500 mt-4 mb-5'>
+                {hash_tags.map((str,i) => <a key={i}>#{str} </a>) }
+            </p>
+            <button className='underline text-xl font-semibold text-[#6047EC]'>Mark as read</button>
         </div>
     );
 };
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -13,8 +13,8 @@ const Blogs = () => {
     console.log(blogs);
 
     return (
-        <div className="w-2/3">
-            <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+        <div className="w-2/3 pr-4">
+            {/* <h1 className="text-4xl">Blogs: {blogs.length}</h1> */}
             {
                 blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
             }
